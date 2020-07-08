b546b3852ae30bd20ed2f3b2eec129cdf7a10c35
Shailsh Nailwal <shailsh.nailwal@sierradev.com>
2020-07-08 15:38:17 +0530

    test colors added

---
+++ MyColors/MyColors/MyColor.swift
@@ -0,0 +1,13 @@
+//
+//  MyColor.swift
+//  MyColors
+//
+//  Created by Shailsh Nailwal on 08/07/20.
+//  Copyright © 2020 Shailsh Nailwal. All rights reserved.
+//
+
+import UIKit
+
+class MyColor: NSObject {
+
+}
