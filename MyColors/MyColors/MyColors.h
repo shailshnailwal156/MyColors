4736d923515009463cdfb71b5a957747073d3699
Shailsh Nailwal <shailsh.nailwal@sierradev.com>
2020-07-08 15:33:45 +0530

    Base project added

---
+++ MyColors/MyColors/MyColors.h
@@ -0,0 +1,19 @@
+//
+//  MyColors.h
+//  MyColors
+//
+//  Created by Shailsh Nailwal on 08/07/20.
+//  Copyright © 2020 Shailsh Nailwal. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for MyColors.
+FOUNDATION_EXPORT double MyColorsVersionNumber;
+
+//! Project version string for MyColors.
+FOUNDATION_EXPORT const unsigned char MyColorsVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <MyColors/PublicHeader.h>
+
+
